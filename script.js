04b612368ab994b7ce753c9ee211bb86e46d2919
Arnob10011 <arnobgomasta41@gmail.com>
2021-08-29 01:57:41 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// program to display time every 3 seconds
+function showTime() {
+  // return new date and time
+  let dateTime = new Date();
+
+  // returns the current local time
+
+  const hour = dateTime.getHours();
+  const min = dateTime.getMinutes();
+  const sec = dateTime.getSeconds();
+
+  const hourEl = document.querySelector(".hour");
+  const minEl = document.querySelector(".min");
+  const secEl = document.querySelector(".sec");
+
+  hourEl.textContent = hour;
+  minEl.textContent = min;
+  secEl.textContent = sec;
+
+  setTimeout(showTime, 1000);
+}
+
+showTime();
